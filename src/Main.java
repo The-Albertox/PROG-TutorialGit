3e9306f9f11b35db6ad17f51e484c07665bd807b
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-04-02 12:10:30 +0100

    feat: add things from the enunciado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundou");
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("ahora numero dame favor porfa");
+        int numero = keyboard.nextInt();
+        System.out.println(numero);
+        keyboard.close();
     }
     
 }
